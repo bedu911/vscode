4ded580871e3c4eba5ec15da69431d74a46cce05
Bedanand sah <bedu9111@gmail.com>
2025-03-13 00:29:00 +0530

    Adding some New Floder

---
+++ Queue/e.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char ch[12];
+    printf("Enter Your Name:");
+    scanf("%s",ch);
+    print("Your Name id :%s",ch);
+    return 0;
+}
---
+++ Stack/d.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the Value of A:");
+    scanf("%d",&a);
+    printf("a=%d",a);
+    return 0;
+}
